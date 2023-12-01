225cf67b3c368da21971cc063bc808594b5ce1ba
teja <tejacheekat481@gmail.com>
2023-12-01 19:31:27 +0530

    completed

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -7,6 +7,40 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+//package com.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/social-media")
 public class SocialMediaController {
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<String> getUserProfile(@PathVariable Long userId) {
+        // TODO: Implement logic to retrieve user profile by userId
+        return new ResponseEntity<>("User profile for userId " + userId, HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<String> createUserProfile(@RequestBody String userProfile) {
+        // TODO: Implement logic to create a new user profile
+        return new ResponseEntity<>("User profile created successfully", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/user/{userId}")
+    public ResponseEntity<String> updateUserProfile(@PathVariable Long userId, @RequestBody String updatedProfile) {
+        // TODO: Implement logic to update user profile by userId
+        return new ResponseEntity<>("User profile updated successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public ResponseEntity<String> deleteUserProfile(@PathVariable Long userId) {
+        // TODO: Implement logic to delete user profile by userId
+        return new ResponseEntity<>("User profile deleted successfully", HttpStatus.OK);
+    }
+
+    // Additional endpoints can be added as needed for your application
 
 }
